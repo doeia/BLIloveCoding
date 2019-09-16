fef9f50782c9b04ba09024bc29e35bbe33fc47fd
sanshi1929 <sanshi1929@gmail.com>
2019-09-16 17:47:18 +0900

    08_事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,60 +7,29 @@ Page({
      data: {
 
      },
-
-     /**
-      * 生命周期函数--监听页面加载
-      */
-     onLoad: function (options) {
-
+     handleBtnClick(){
+          console.log('按钮发生')
+     },
+     onTouchStart(e){
+          console.log('触摸开始手势', e)
+     },
+     onTouchMove(e) {
+          console.log('触摸移动手势', e)
+     },
+     onTouchEnd(e) {
+          console.log('触摸结束手势', e)
+     },
+     onTouchCancel(e){
+          console.log('触摸取消', e)
+     },
+     onTap(e) {
+          console.log('轻点', e)
+     },
+     onLongPress(e){
+          console.log('长按手势', e)
      },
 
-     /**
-      * 生命周期函数--监听页面初次渲染完成
-      */
-     onReady: function () {
-
-     },
-
-     /**
-      * 生命周期函数--监听页面显示
-      */
-     onShow: function () {
-
-     },
-
-     /**
-      * 生命周期函数--监听页面隐藏
-      */
-     onHide: function () {
-
-     },
-
-     /**
-      * 生命周期函数--监听页面卸载
-      */
-     onUnload: function () {
-
-     },
-
-     /**
-      * 页面相关事件处理函数--监听用户下拉动作
-      */
-     onPullDownRefresh: function () {
-
-     },
-
-     /**
-      * 页面上拉触底事件的处理函数
-      */
-     onReachBottom: function () {
-
-     },
-
-     /**
-      * 用户点击右上角分享
-      */
-     onShareAppMessage: function () {
-
+     handleEventClick(event){
+          console.log('-------', event)
      }
 })
