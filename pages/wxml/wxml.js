194df2c258802683f3b987733ecc9026ee9f3188
sanshi1929 <sanshi1929@gmail.com>
2019-09-16 17:12:45 +0900

    07_wxss_wxml_wxs

---
+++ pages/wxml/wxml.js
@@ -0,0 +1,88 @@
+// pages/wxml/wxml.js
+Page({
+
+     /**
+      * 页面的初始数据
+      */
+     data: {
+          message:'你好小程序',
+          firstname: 'kobe',
+          lastname: 'bryant',
+          age:20,
+          nowTime: new Date().toLocaleTimeString(),
+          isActive:false,
+          isShow: true
+     },
+
+     /**
+      * 生命周期函数--监听页面加载
+      */
+     onLoad: function (options) {
+          setInterval(()=>{
+               this.setData({
+                    nowTime: new Date().toLocaleTimeString()
+               })
+          },1000)
+     },
+
+     handleSwitchColor(){
+          this.setData({
+               isActive:!this.data.isActive
+          })
+     },
+
+     handleSwitchShow(){
+          this.setData({
+               isShow: !this.data.isShow
+          })
+     },
+
+     /**
+      * 生命周期函数--监听页面初次渲染完成
+      */
+     onReady: function () {
+
+     },
+
+     /**
+      * 生命周期函数--监听页面显示
+      */
+     onShow: function () {
+
+     },
+
+     /**
+      * 生命周期函数--监听页面隐藏
+      */
+     onHide: function () {
+
+     },
+
+     /**
+      * 生命周期函数--监听页面卸载
+      */
+     onUnload: function () {
+
+     },
+
+     /**
+      * 页面相关事件处理函数--监听用户下拉动作
+      */
+     onPullDownRefresh: function () {
+
+     },
+
+     /**
+      * 页面上拉触底事件的处理函数
+      */
+     onReachBottom: function () {
+
+     },
+
+     /**
+      * 用户点击右上角分享
+      */
+     onShareAppMessage: function () {
+
+     }
+})
