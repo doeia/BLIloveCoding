134eef1a66ef254e7a71e2408b2774cdf4141d77
sanshi1929 <sanshi1929@gmail.com>
2019-09-17 12:45:44 +0900

    10_网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,26 @@
 // pages/home/home.js
-Page({
+import request from '../../service/network.js'
 
-     /**
-      * 页面的初始数据
-      */
+Page({
      data: {
 
      },
-
-     /**
-      * 生命周期函数--监听页面加载
-      */
      onLoad: function (options) {
+          //1. 原生的方式发送网络请求
+          this.get_data_origin()
 
+          //2. 使用封装的request发送网络请求
+          //Promise最大的好处就是防止回调地狱
+          request({
+               url:'http://123.207.32.32:8000/api/v1/recommend'
+          }).then(res =>{
+               console.log(res)
+          }).catch(err => {
+               console.log(err)
+          })
      },
 
-     /**
-      * 生命周期函数--监听页面初次渲染完成
-      */
-     onReady: function () {
-
-     },
-
-     /**
-      * 生命周期函数--监听页面显示
-      */
-     onShow: function () {
-
-     },
-
-     /**
-      * 生命周期函数--监听页面隐藏
-      */
-     onHide: function () {
-
-     },
-
-     /**
-      * 生命周期函数--监听页面卸载
-      */
-     onUnload: function () {
-
-     },
-
-     /**
-      * 页面相关事件处理函数--监听用户下拉动作
-      */
-     onPullDownRefresh: function () {
-
-     },
-
-     /**
-      * 页面上拉触底事件的处理函数
-      */
-     onReachBottom: function () {
-
-     },
-
-     /**
-      * 用户点击右上角分享
-      */
-     onShareAppMessage: function () {
+     get_data_origin(){
 
      }
 })
---
+++ service/network.js
@@ -0,0 +1,17 @@
+export default function request(options){
+     return new Promise((resolve, reject) => {
+          wx.request({
+               url: options.url,
+               method: options.method || 'get',
+               data: options.data || {},
+               success: function (res) {
+                    resolve(res)
+               },
+               fial: function (err) {
+                    reject(err)
+               }
+
+          })
+     })
+}
+
